d39a0ee61152c588ae430b18b842a9c54d0d4577
Esko Luontola <esko@luontola.fi>
2020-12-12 04:55:14 +0200

    Print GC stats

--- src/hello_world/main.clj
+++ src/hello_world/main.clj
@@ -3,12 +3,13 @@
   (:import (com.amazonaws.services.lambda.runtime Context)
            (com.amazonaws.services.lambda.runtime.api.client AWSLambda)
            (java.io InputStream OutputStream)
-           (java.lang.management ManagementFactory))
+           (java.lang.management ManagementFactory GarbageCollectorMXBean))
   (:gen-class))
 
 (lambada/deflambdafn hello_world.Handler [^InputStream in ^OutputStream out ^Context ctx]
   (println "Hello world")
   (println (slurp in))
+  (println "JVM:")
   (println "uptime" (.getUptime (ManagementFactory/getRuntimeMXBean)) "ms")
   (doseq [k ["java.specification.version"
              "java.version"
@@ -18,7 +19,10 @@
              "java.vendor.version"]]
     (println k "=" (System/getProperty k)))
   #_(doseq [[k v] (sort-by key (System/getProperties))]
-      (prn k v)))
+      (prn k v))
+  (println "GC:")
+  (doseq [^GarbageCollectorMXBean bean (ManagementFactory/getGarbageCollectorMXBeans)]
+    (println (.getName bean) "-" (.getCollectionCount bean) "collections, time spent" (.getCollectionTime bean) "ms")))
 
 (defn -main [& _args]
   (AWSLambda/main (into-array String ["hello_world.Handler"])))
